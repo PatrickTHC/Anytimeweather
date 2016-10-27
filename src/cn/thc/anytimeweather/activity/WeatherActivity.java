7095bdea97e6e223603246b3aa6aa807a9d4f921
PatrickTHC <1061791852@qq.com>
2016-10-28 00:30:57 +0800

    新增切换城市和手动更新天气的功能

--- src/cn/thc/anytimeweather/activity/WeatherActivity.java
+++ src/cn/thc/anytimeweather/activity/WeatherActivity.java
@@ -1,6 +1,7 @@
 package cn.thc.anytimeweather.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -8,7 +9,9 @@ import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import cn.thc.anytimeweather.R;
@@ -16,7 +19,7 @@ import cn.thc.anytimeweather.util.HttpCallbackListener;
 import cn.thc.anytimeweather.util.HttpUtil;
 import cn.thc.anytimeweather.util.Utility;
 
-public class WeatherActivity extends Activity {
+public class WeatherActivity extends Activity implements OnClickListener {
 
 	private LinearLayout weatherInfoLayout;
 	private TextView cityNameText;
@@ -25,6 +28,8 @@ public class WeatherActivity extends Activity {
 	private TextView temp1Text;
 	private TextView temp2Text;
 	private TextView currentDateText;
+	private Button switchCity;
+	private Button refreshWeather;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +52,34 @@ public class WeatherActivity extends Activity {
 		} else {
 			showWeather();
 		}
+		switchCity = (Button) findViewById(R.id.switch_city);
+		refreshWeather = (Button) findViewById(R.id.refresh_weather);
+		switchCity.setOnClickListener(this);
+		refreshWeather.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.switch_city:
+			Intent intent = new Intent(this, ChooseAreaActivity.class);
+			intent.putExtra("from_weather_activity", true);
+			startActivity(intent);
+			finish();
+			break;
+
+		case R.id.refresh_weather:
+			publishText.setText("Í¬²½ÖÐ...");
+			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+			String weatherCode = prefs.getString("weather_code", "");
+			if (!TextUtils.isEmpty(weatherCode)) {
+				queryWeatherInfo(weatherCode);
+			}
+			break;
+		default:
+			break;
+		}
+
 	}
 
 	private void queryWeatherCode(String countyCode) {
@@ -123,4 +156,5 @@ public class WeatherActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
 }
